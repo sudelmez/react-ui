3dfcdb48f98bba5150dbd4621047c140714ad792
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2024-06-14 16:09:27 +0300

    protected route

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import AuthPage from './pages/auth/auth';
 import Userpage from './pages/user/user_view';
 import AddUserPage from './pages/add_user/add_user';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/home/home';
-import AuthProvider from './hooks/auth-provider';
+import { useAuth } from './hooks/auth-provider';
 
 function App() {
-  const [user, setUser] = useState();
+  const { user } = useAuth();
   return (
-    <AuthProvider value={{ user }} >
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/auth" />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/user" element={<Userpage />} />
-          <Route path="/addUser" element={<AddUserPage />} />
-          <Route
-            path="/home"
-            element={
-              user ? (
-                <HomePage user={user} />
-              ) : (
-                <Navigate to="/auth" replace />
-              )
-            }
-          />
-        </Routes>
-      </Router>
-    </AuthProvider>
-
+    <Router>
+      <Routes>
+        {!user ? (
+          <>
+            <Route path="*" element={<Navigate to="/auth" />} />
+            <Route path="/auth" element={<AuthPage />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/user" element={<Userpage />} />
+            <Route path="/addUser" element={<AddUserPage />} />
+          </>
+        )}
+      </Routes>
+    </Router>
   );
 }
 
